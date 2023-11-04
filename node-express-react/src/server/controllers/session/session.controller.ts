edf9bd2bf1794a4c24eb063ec82a09a4d797ff0f
rswdch <ryan.sawadichai@outlook.com>
2023-11-03 21:13:52 -0600

    switched user and session

--- node-express-react/src/server/controllers/session/session.controller.ts
+++ node-express-react/src/server/controllers/session/session.controller.ts
@@ -6,8 +6,6 @@ import * as dotenv from "dotenv";
 dotenv.config();
 const SALT_ROUNDS: number = Number(process.env.SALT_ROUNDS);
 
-export async function getAllUsers(req: Request, res: Response, next: NextFunction) {
-  const rows = await db.selectFrom("users").select("username").execute();
-  res.locals.users = rows;
+export async function login(req: Request, res: Response, next: NextFunction) {
   next();
 }
